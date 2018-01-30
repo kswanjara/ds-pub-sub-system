987afb28c680199affb5da193202d6ad655b915a
kunalwanjara <ksw5002@rit.edu>
2018-01-30 16:35:42 -0500

    Basic connection code for publisher and subscriber.

--- src/ds/project1/commondtos/Topic.java
+++ src/ds/project1/commondtos/Topic.java
@@ -6,4 +6,21 @@ public class Topic {
 	private int id;
 	private List<String> keywords;
 	private String name;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public List<String> getKeywords() {
+		return keywords;
+	}
+
+	public void setKeywords(List<String> keywords) {
+		this.keywords = keywords;
+	}
+
 }
--- src/ds/project1/eventmanager/EventManager.java
+++ src/ds/project1/eventmanager/EventManager.java
@@ -1,15 +1,22 @@
 package ds.project1.eventmanager;
 
+import ds.project1.commondtos.ConnectionDetails;
 import ds.project1.commondtos.Event;
 import ds.project1.commondtos.Topic;
 
-public class EventManager {
+public class EventManager implements CallBack {
+
+	@Override
+	public void newConnection(ConnectionDetails connectionDetails) {
+		System.out.println("Got new connection details!");
+	}
 
 	/*
 	 * Start the repo service
 	 */
 	private void startService() {
-
+		Thread connectionThread = new Thread(new ConnectionManager(new EventManager()));
+		connectionThread.start();
 	}
 
 	/*
@@ -49,6 +56,9 @@ public class EventManager {
 
 	public static void main(String[] args) {
 		new EventManager().startService();
+		while(true) {
+			
+		}
 	}
 
 }
