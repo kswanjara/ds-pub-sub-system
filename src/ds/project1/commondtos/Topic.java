c4855f352291ec7abc8a73e6efaf1c9fb480617d
Rohit Keswani <rk5075@rit.edu>
2018-02-02 18:59:56 -0500

    Added the getter and setter's for the name

--- src/ds/project1/commondtos/Topic.java
+++ src/ds/project1/commondtos/Topic.java
@@ -15,6 +15,14 @@ public class Topic {
 		this.id = id;
 	}
 
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
 	public List<String> getKeywords() {
 		return keywords;
 	}
