816b75552e77aa8c35ba08e4cee3aac8d10ed840
kswanjara <ksw5002@rit.edu>
2018-02-04 09:42:15 -0500

    Added serialID

--- src/ds/project1/commondtos/Packet.java
+++ src/ds/project1/commondtos/Packet.java
@@ -6,62 +6,63 @@ import ds.project1.eventmanager.dto.SubscriberDto;
 import java.io.Serializable;
 import java.util.List;
 
-public class Packet implements Serializable{
-    private Topic topic;
-    private Event event;
-    private String type;
-    private AbstractPubSubDto abstractPubSubDto;
-    private List<Topic> TopicList;
+public class Packet implements Serializable {
+	private static final long serialVersionUID = -5741160726800185379L;
 
+	private Topic topic;
+	private Event event;
+	private String type;
+	private AbstractPubSubDto abstractPubSubDto;
+	private List<Topic> TopicList;
 
-    public List<Topic> getTopicList() {
-        return TopicList;
-    }
+	public List<Topic> getTopicList() {
+		return TopicList;
+	}
 
-    public void setTopicList(List<Topic> topicList) {
-        TopicList = topicList;
-    }
+	public void setTopicList(List<Topic> topicList) {
+		TopicList = topicList;
+	}
 
-    public Packet(){
+	public Packet() {
 
-    }
+	}
 
-    public Packet(Topic topic, Event event, String type, SubscriberDto subscriberDto) {
-        this.event = event;
-        this.topic = topic;
-        this.type = type;
-        this.abstractPubSubDto = subscriberDto;
-    }
+	public Packet(Topic topic, Event event, String type, SubscriberDto subscriberDto) {
+		this.event = event;
+		this.topic = topic;
+		this.type = type;
+		this.abstractPubSubDto = subscriberDto;
+	}
 
-    public Topic getTopic() {
-        return topic;
-    }
+	public Topic getTopic() {
+		return topic;
+	}
 
-    public void setTopic(Topic topic) {
-        this.topic = topic;
-    }
+	public void setTopic(Topic topic) {
+		this.topic = topic;
+	}
 
-    public Event getEvent() {
-        return event;
-    }
+	public Event getEvent() {
+		return event;
+	}
 
-    public void setEvent(Event event) {
-        this.event = event;
-    }
+	public void setEvent(Event event) {
+		this.event = event;
+	}
 
-    public String getType() {
-        return type;
-    }
+	public String getType() {
+		return type;
+	}
 
-    public void setType(String type) {
-        this.type = type;
-    }
+	public void setType(String type) {
+		this.type = type;
+	}
 
-    public AbstractPubSubDto getAbstractPubSubDto() {
-        return abstractPubSubDto;
-    }
+	public AbstractPubSubDto getAbstractPubSubDto() {
+		return abstractPubSubDto;
+	}
 
-    public void setAbstractPubSubDto(AbstractPubSubDto abstractPubSubDto) {
-        this.abstractPubSubDto = abstractPubSubDto;
-    }
+	public void setAbstractPubSubDto(AbstractPubSubDto abstractPubSubDto) {
+		this.abstractPubSubDto = abstractPubSubDto;
+	}
 }
